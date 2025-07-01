c3a02d96484947b59044583c81276cf59d8bde86
Márcio Corrêa <marcio.ramos.correa@gmail.com>
2025-06-30 22:24:52 -0300

    utilizando os 3 As

--- src/comunicacao.js
+++ src/comunicacao.js
@@ -1,6 +1,6 @@
 function comunicacaoDosAnimais(nomeDoAnimal) {
     if (nomeDoAnimal === 'cachorro') {
-        return 'au au'
+        return 'woff woff'
     } else if (nomeDoAnimal === 'gato') {
         return 'miau'
     } else {
--- test/comunicacao.test.js
+++ test/comunicacao.test.js
@@ -13,15 +13,30 @@ const { comunicacaoDosAnimais } = require('../src/comunicacao.js')
 
 // função anomina - não tem nome e nem parametro. Permite a execução de mais comandos dentro da função
 describe('Comunicacao', function() {
-    it('Validar que Cachorro se Comunica com au au', function() {
-        expect(comunicacaoDosAnimais('cachorro')).to.eq('au au')
+    it('Validar que Cachorro se Comunica com woff woff', function() {
+        //Arrange
+        const animal = 'cachorro'
+        //Act
+        const comunicacao = comunicacaoDosAnimais(animal)
+        //Assert
+        expect(comunicacao).to.eq('woff woff')
     })
 
     it('Validar que Gato se Comunica com miau', function() {
-        expect(comunicacaoDosAnimais('gato')).to.eq('miau')
+        //Arrange
+        const animal = 'gato'
+        //Act
+        const comunicacao = comunicacaoDosAnimais(animal)
+        //Assert
+        expect(comunicacao).to.eq('miau')
     })
 
     it('Validar que Papagaio retorna invalido', function() {
-        expect(comunicacaoDosAnimais('papagaio')).to.eq('invalido')
+        //Arrange
+        const animal = 'papagaio'
+        //Act
+        const comunicacao = comunicacaoDosAnimais(animal)
+        //Assert
+        expect(comunicacao).to.eq('inválido')
     })
 })
